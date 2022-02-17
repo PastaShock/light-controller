6555f1c01cac3ec5b67d44cc29c05f72d330e752
George Pastushok <georgepastushok@gmail.com>
2022-02-17 18:00:50 +0000

    changed tab spacing, fixed HTML tag opening and closing tags

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <head>
-<link rel=stylesheet type="text/css" href="style.css">
+	<link rel=stylesheet type="text/css" href="style.css">
 </head>
 <body>
-<h1>Light controller</h1>
-<div class='title area'>
-	<p>Toggle the lights on and off:</p>
-</div>
-<div class='switch panel' onclick='Switch()'>
-	<form action="action.php" method="POST">
-	<label>
-	lights: 
-	<input type="range" id="lightswitch" name="lightswitch" min="0" max="1" />
-	</label>
-</div>
-<button class="submit" id="phpButton">Submit</button>
-</form>
-<script>
-var switch = document.getElementById('lightswitch');
-function Switch() {
-	if (switch.value == 1) {
-		switch.value = 0;
-	} else {
-		switch.value = 1;
-	}
-	switch.style.display = 'none';
-	switch.style.display = 'block';
-}
-</script>
+	<h1>Light controller</h1>
+	<div class='title area'>
+		<p>Toggle the lights on and off:</p>
+	</div>
+	<div class='switch panel' onclick='Switch()'>
+		<form action="action.php" method="POST">
+			<label>
+				lights: 
+				<input type="range" id="lightswitch" name="lightswitch" min="0" max="1" />
+			</label>
+		</form>
+	</div>
+	<button class="submit" id="phpButton">Submit</button>
+	<script>
+		var switch = document.getElementById('lightswitch');
+		function Switch() {
+			if (switch.value == 1) {
+					switch.value = 0;
+				} else {
+					switch.value = 1;
+			}
+			switch.style.display = 'none';
+			switch.style.display = 'block';
+		}
+	</script>
 </body>
